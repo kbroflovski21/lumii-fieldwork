82becfd0d09307d9c0d5484c2414c3516416cb10
mexy <kevinf@ezblock.io>
2026-05-17 15:05:34 +0000

    deploy: add family H5 mobile page to GitHub Pages (built)

--- pages/family/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import FamilyPage from './FamilyPage';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <FamilyPage />
-  </React.StrictMode>,
-);
